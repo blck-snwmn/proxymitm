c841cddd60a861a42797c608738993cd86b7bc70
blck-snwmn <whitesnowmancreator@gmail.com>
2019-08-08 20:27:42 +0900

    Change to proxy request

--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 )
+
+func proxy(w http.ResponseWriter, r *http.Request) {
+	dest, err := net.Dial("tcp", r.RequestURI)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
+	hjk, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Not available http.Hijacker", http.StatusInternalServerError)
+		return
+	}
+	con, _, err := hjk.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
+	//コネクションが張れたため、200 を返す
+	// ハイジャックをしているため w.WriteHeader 使えない
+	con.Write([]byte("HTTP/1.0 200 Connection established"))
+	con.Write([]byte("\r\n\r\n"))
+
+	go transfer(dest, con)
+	go transfer(con, dest)
+}
+
+//dest -> drc へデータを渡す
+func transfer(dest io.WriteCloser, src io.ReadCloser) {
+	defer dest.Close()
+	defer src.Close()
+	io.Copy(dest, src)
+}
 
 func main() {
 	server := &http.Server{
@@ -13,9 +48,10 @@ func main() {
 			// curl 等で proxyの設定をして、アクセスすると
 			// https の場合、`aa` を出力していることが分かる
 			if r.Method == http.MethodConnect {
-				fmt.Println("aa")
+				log.Println("aa")
+				proxy(w, r)
 			} else {
-				fmt.Println("bb")
+				log.Println("bb")
 			}
 		}),
 	}

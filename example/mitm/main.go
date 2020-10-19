83ae6963e5722d35aea0589c019decb6eac000ee
blck-snwmn <whitesnowmancreator@gmail.com>
2020-10-20 02:55:45 +0900

    Chamge package

---
+++ example/mitm/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/blck-snwmn/proxymitm"
+)
+
+func main() {
+	mp, err := proxymitm.CreateMitmProxy("./testdata/ca.crt", "./testdata/ca.key")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	server := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// curl 等で proxyの設定をして、アクセスすると
+			// https の場合、`aa` を出力していることが分かる
+			if r.Method == http.MethodConnect {
+				log.Println("aa")
+				mp.Handler(w, r)
+				// proxy(w, r)
+			} else {
+				log.Println("bb")
+			}
+		}),
+	}
+
+	log.Println(server.ListenAndServe())
+}
--- mitmproxxy.go
+++ mitmproxxy.go
@@ -1,4 +1,4 @@
-package main
+package proxymitm
 
 import (
 	"bufio"
